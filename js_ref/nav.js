55a49c12b48271949d415c316a3add9579ba9043
nobodyP <117957170+nobodyP@users.noreply.github.com>
2026-03-04 16:42:32 -0600

    add a bunch of shit from jonahs website.

---
+++ js_ref/nav.js
@@ -0,0 +1,8 @@
+document.write('<ul>');
+document.write('<li><a href="index.html">Home</a></li>');
+document.write('<li><a href="about.html">About</a></li>');
+document.write('<li><a href="https://www.youtube.com/channel/UCbscCHSeDcMd1mggSoMl5Mg">Youtube</a></li>');
+document.write('<li><a href="python_code.html">Python Code</a></li>');
+document.write('<li><a href="art.html">Art</a></li>');
+document.write('<li><a href="gaming.html">Video Games</a></li>');
+document.write('</ul>');
