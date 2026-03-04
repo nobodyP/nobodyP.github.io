a56cf7a4a35587e3f348d1df27dcc7c759e549df
nobodyP <117957170+nobodyP@users.noreply.github.com>
2026-03-04 17:26:19 -0600

    fix footer

--- js_ref/footer.js
+++ js_ref/footer.js
@@ -1,4 +1,5 @@
 document.write('<figure>');
+document.write('<a href="https://psychicdigit.neocities.org/">');
+document.write('<img src="Images/Buttons/webzone_resized.gif">');
 document.write('</a>');
-document.write('<img src="./Images/Buttons/under_construction.gif">');
 document.write('</figure>');
