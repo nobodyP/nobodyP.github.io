f08bdbe6ded3ceca21cdf6ff18158236bb17fc3a
nobodyP <117957170+nobodyP@users.noreply.github.com>
2026-04-01 16:35:04 -0500

    destroy ai unless it is doing work for me...

--- js_ref/footer.js
+++
@@ -1,5 +0,0 @@
-document.write('<figure>');
-document.write('<a href="https://psychicdigit.neocities.org/">');
-document.write('<img src="Images/Buttons/webzone_resized.gif">');
-document.write('</a>');
-document.write('</figure>');
--- js_ref/nav.js
+++
@@ -1,5 +0,0 @@
-document.write('<ul>');
-document.write('<li><a href="index.html">Home</a></li>');
-document.write('<li><a href="about.html">About</a></li>');
-document.write('<li><a href="https://www.youtube.com/@nobodyremix">Youtube</a></li>');
-document.write('</ul>');
